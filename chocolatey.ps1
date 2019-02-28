b9bcfead8bd6c6c3cad294224f1405ad84207a09
potateros <ericngu97@gmail.com>
2019-02-28 14:09:34 +0800

    moved files, updated keybinds, added chocolatey ps script

---
+++ chocolatey.ps1
@@ -0,0 +1,3 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install -y git github-desktop googlechrome firefox notepadplusplus 7zip sumatrapdf
